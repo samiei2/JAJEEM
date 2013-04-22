5c2836e1f893e7d2e502214dfe018ac314256162
armin <samiei2@gmail.com>
2013-04-22 16:15:27 +0430

    SeatDAO Implementation Complete - ISeatDAO corrected

--- src/com/jajeem/room/dao/ISeatDAO.java
+++ src/com/jajeem/room/dao/ISeatDAO.java
@@ -1,10 +1,15 @@
 package com.jajeem.room.dao;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import com.jajeem.room.model.Seat;
+
 public interface ISeatDAO {
 	
-	void create();
-	void update();
-	void delete();
-	void get();
-	void list();
+	void create(Seat seat) throws SQLException;
+	void update(Seat seat) throws SQLException;
+	void delete(Seat seat) throws SQLException;
+	Seat get(int id) throws SQLException;
+	ArrayList<Seat> list() throws SQLException;
 }
