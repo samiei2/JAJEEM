bc5dc2b0637743c4f7ccb0fcbfb6107b7d7c6481
armin <samiei2@gmail.com>
2013-04-22 17:29:13 +0430

    ResponseDAO Implementation Complete - IResponseDAO corrected

--- src/com/jajeem/survey/dao/IResponseDAO.java
+++ src/com/jajeem/survey/dao/IResponseDAO.java
@@ -1,9 +1,14 @@
 package com.jajeem.survey.dao;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import com.jajeem.survey.model.Response;
+
 public interface IResponseDAO {
 	
-	void create();
-	void update();
-	void get();
-	void list();
+	void create(Response respons) throws SQLException;
+	void update(Response respons) throws SQLException;
+	Response get(int id) throws SQLException;
+	ArrayList<Response> list() throws SQLException;
 }
