01fb48b08c7404b46d75b7b76005b619cf1b5c66
Mohammad <smh.tahaei@gmail.com>
2013-04-24 11:13:46 +0430

    Instructor H2 DAO imp

---
+++ src/com/jajeem/core/dao/h2/InstructorDAO.java
@@ -0,0 +1,293 @@
+package com.jajeem.core.dao.h2;
+
+import java.sql.*;
+import java.util.ArrayList;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+
+import com.jajeem.util.H2ConnectionImpl;
+
+import com.jajeem.core.dao.IInstructorDAO;
+import com.jajeem.core.model.*;
+
+public class InstructorDAO implements IInstructorDAO {
+
+	H2ConnectionImpl conn = new H2ConnectionImpl();
+	Logger logger = Logger.getLogger(InstructorDAO.class);
+
+	public InstructorDAO() {
+		PropertyConfigurator.configure("conf/log4j.conf");
+	}
+
+	@Override
+	public Instructor authenticate(String username, String password)
+			throws SQLException {
+
+		String query = "SELECT * FROM Instructor WHERE Instructor.username = '%s';";
+
+		Connection con = conn.getConnection();
+		ResultSet rs = null;
+		Instructor instructor = new Instructor();
+
+		// checking...
+		if (username.length() == 0) {
+			logger.error("authenticate: failed ... invalid username");
+			return instructor;
+		}
+
+		if (password.length() == 0) {
+			logger.error("authenticate: failed ... invalid password");
+			return instructor;
+		}
+
+		String.format(query, username);
+
+		try (Statement statement = con.createStatement()) {
+			rs = statement.executeQuery(query);
+			if (rs.next()) {
+				if (rs.getString(2).compareTo(rs.getString(3)) == 0) {
+					logger.debug("authenticate: matched instructor's password [instructor no="
+							+ username + "]");
+					instructor.setId(rs.getInt(1));
+				} else {
+					logger.debug("authenticate: didn't match instructor's password [instructor no="
+							+ username + "]");
+					instructor.setId(0);
+				}
+			} else {
+				logger.debug("authenticate: didn't match instructor no ... [instructor no="
+						+ username + "]");
+			}
+		} catch (SQLException e) {
+			logger.error("authenticate: throwed an exception: "
+					+ e.getMessage());
+			e.printStackTrace();
+			instructor.setId(-1);
+		} finally {
+			try {
+				if (con != null)
+					con.close();
+			} catch (Exception e) {
+			}
+		}
+		return instructor;
+	}
+
+	@Override
+	public Instructor create(Instructor instructor) throws SQLException {
+
+		String query = String
+				.format("INSERT INTO Instructor (firstName, middleName, lastName, username, password, language)"
+						+ " VALUES ('%s', '%s', '%s', '%s', '%s', '%s');",
+						instructor.getFirstName(), instructor.getMiddleName(),
+						instructor.getLastName(), instructor.getUsername(),
+						instructor.getPassword(), instructor.getLanguage());
+
+		Connection con = conn.getConnection();
+		int rs = 0;
+
+		try (Statement statement = con.createStatement()) {
+			rs = statement.executeUpdate(query);
+			// need to add id to instructor obj
+		} catch (SQLException e) {
+			e.printStackTrace();
+			instructor.setId(-1);
+		} finally {
+			try {
+				if (rs == 1) {
+
+				}
+			} catch (Exception e) {
+			}
+			try {
+				if (con != null)
+					con.close();
+			} catch (Exception e) {
+			}
+		}
+		
+		try {
+			if (con != null)
+				con.close();
+		} catch (Exception e) {
+		}
+		
+		return instructor;
+	}
+
+	@Override
+	public boolean update(Instructor instructor) throws SQLException {
+
+		String query = String
+				.format("UPDATE Instructor SET firstName='%s', middleName='%s', lastName='%s', "
+						+ " username='%s', password='%s', language='%s' WHERE id = '%d'",
+						instructor.getFirstName(), instructor.getMiddleName(),
+						instructor.getLastName(), instructor.getUsername(),
+						instructor.getPassword(), instructor.getLanguage(),
+						instructor.getId());
+
+		Connection con = conn.getConnection();
+		int rs = 0;
+
+		try (Statement statement = con.createStatement()) {
+			rs = statement.executeUpdate(query);
+		} catch (SQLException e) {
+			e.printStackTrace();
+			instructor.setId(-1);
+		} finally {
+			try {
+				if (rs == 1) {
+					return true;
+				} else {
+					return false;
+				}
+			} catch (Exception e) {
+			}
+			try {
+				if (con != null)
+					con.close();
+			} catch (Exception e) {
+			}
+		}
+
+		try {
+			if (con != null)
+				con.close();
+		} catch (Exception e) {
+		}
+		
+		return false;
+	}
+
+	@Override
+	public boolean delete(Instructor instructor) throws SQLException {
+		String query = String.format(
+				"DELETE FROM Instructor WHERE Instructor.id = '%d';",
+				instructor.getId());
+
+		Connection con = conn.getConnection();
+		int rs = 0;
+
+		try (Statement statement = con.createStatement()) {
+			rs = statement.executeUpdate(query);
+		} catch (SQLException e) {
+			e.printStackTrace();
+			instructor.setId(-1);
+		} finally {
+			try {
+				if (rs == 1) {
+					return true;
+				} else {
+					return false;
+				}
+			} catch (Exception e) {
+			}
+			try {
+				if (con != null)
+					con.close();
+			} catch (Exception e) {
+			}
+		}
+		
+		try {
+			if (con != null)
+				con.close();
+		} catch (Exception e) {
+		}
+
+		return false;
+	}
+
+	@Override
+	public Instructor get(Instructor instructor) throws SQLException {
+		String query = String.format("SELECT * FROM Instructor WHERE Instructor.id = %d;", instructor.getId());
+
+		Connection con = conn.getConnection();
+		ResultSet rs = null;
+
+		try (Statement statement = con.createStatement()) {
+			rs = statement.executeQuery(query);
+			if (rs.next()) {
+				instructor.setFirstName(rs.getString("firstName"));
+				instructor.setMiddleName(rs.getString("middleName"));
+				instructor.setLastName(rs.getString("lastName"));
+				instructor.setUsername(rs.getString("username"));
+				instructor.setPassword(rs.getString("password"));
+				instructor.setLanguage(rs.getString("language"));
+			} else {
+				instructor.setId(0);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			instructor.setId(-1);
+		} finally {
+			try {
+				if (rs != null)
+					rs.close();
+			} catch (Exception e) {
+			}
+			try {
+				if (con != null)
+					con.close();
+			} catch (Exception e) {
+			}
+		}
+		
+		try {
+			if (con != null)
+				con.close();
+		} catch (Exception e) {
+		}
+
+		return instructor;
+	}
+
+	@Override
+	public ArrayList<Instructor> list() throws SQLException {
+		String query = "SELECT * FROM Instructor";
+		ArrayList<Instructor> allInstructors = new ArrayList<>();
+
+		Connection con = conn.getConnection();
+		ResultSet rs = null;
+
+		try (Statement statement = con.createStatement()) {
+			rs = statement.executeQuery(query);
+			while (rs.next()) {
+				Instructor instructor = new Instructor();
+
+				instructor.setId(rs.getInt("id"));
+				instructor.setFirstName(rs.getString("firstName"));
+				instructor.setMiddleName(rs.getString("middleName"));
+				instructor.setLastName(rs.getString("lastName"));
+				instructor.setUsername(rs.getString("username"));
+				instructor.setPassword(rs.getString("password"));
+				instructor.setLanguage(rs.getString("language"));
+
+				allInstructors.add(instructor);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (rs != null)
+					rs.close();
+			} catch (Exception e) {
+			}
+			try {
+				if (con != null)
+					con.close();
+			} catch (Exception e) {
+			}
+		}
+		
+		try {
+			if (con != null)
+				con.close();
+		} catch (Exception e) {
+		}
+		
+		return allInstructors;
+	}
+
+}
---
+++ src/com/jajeem/core/dao/h2/StudentDAO.java
@@ -0,0 +1,275 @@
+package com.jajeem.core.dao.h2;
+
+import java.sql.*;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+
+import com.jajeem.util.HSQLDBConnectionImpl;
+
+import com.jajeem.core.dao.IStudentDAO;
+import com.jajeem.core.model.*;
+
+public class StudentDAO implements IStudentDAO {
+
+	HSQLDBConnectionImpl conn = new HSQLDBConnectionImpl();
+	Logger logger = Logger.getLogger(StudentDAO.class);
+
+	public StudentDAO () {
+		PropertyConfigurator.configure("conf/log4j.conf");
+	}
+
+	@Override
+	public Student authenticate(String username, String password) {
+
+		Student student = new Student();
+
+		// checking...
+		if (username.length() == 0) {
+			logger.error("authenticate: failed ... invalid username");
+			return student;
+		}
+
+		if (password.length() == 0) {
+			logger.error("authenticate: failed ... invalid password");
+			return student;
+		}
+
+		// create connection
+		Connection con = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+
+		try {
+			// get connection
+			con = conn.getConnection();
+			ps = con.prepareStatement("SELECT id FROM Student WHERE username=:1");
+			ps.setString(1, username);
+			rs = ps.executeQuery();
+			if (rs.next()) {
+				if (rs.getString(2).compareTo(rs.getString(3)) == 0) {
+					logger.debug("authenticate: matched student's password [student no="
+							+ username + "]");
+					student.setId(rs.getInt(1));
+				} else {
+					logger.debug("authenticate: didn't match student's password [student no="
+							+ username + "]");
+					student.setId(0);
+				}
+			} else {
+				logger.debug("authenticate: didn't match student no ... [student no="
+						+ username + "]");
+			}
+		} catch (SQLException e) {
+			logger.error("authenticate: throwed an exception: "
+					+ e.getMessage());
+			e.printStackTrace();
+			student.setId(-1);
+		} finally {
+			try {
+				if (rs != null)
+					rs.close();
+			} catch (Exception e) {
+			}
+			try {
+				if (ps != null)
+					ps.close();
+			} catch (Exception e) {
+			}
+			try {
+				if (con != null)
+					con.close();
+			} catch (Exception e) {
+			}
+		}
+		return student;
+	}
+
+	@Override
+	public Student create(Student student) {
+		Connection con = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+
+		try {
+			con = conn.getConnection();
+
+			ps = con.prepareStatement("INSERT INTO Student VALUES( firstName=:1, middleName=:1, lastName=:1, "
+					+ "username=:1, password=:1, langauge=:1");
+			ps.setString(1, student.getFirstName());
+			ps.setString(2, student.getMiddleName());
+			ps.setString(3, student.getLastName());
+			ps.setString(4, student.getUsername());
+			ps.setString(5, student.getPassword());
+			ps.setString(6, student.getLangauge());
+
+			rs = ps.executeQuery();
+
+			if (rs.next()) {
+				student.setId(rs.getInt(1));
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+			student.setId(-1);
+		} finally {
+			try {
+				if (rs != null)
+					rs.close();
+			} catch (Exception e) {
+			}
+			try {
+				if (ps != null)
+					ps.close();
+			} catch (Exception e) {
+			}
+			try {
+				if (con != null)
+					con.close();
+			} catch (Exception e) {
+			}
+		}
+
+		return student;
+	}
+
+	@Override
+	public boolean update(Student student) {
+		Connection con = null;
+		PreparedStatement ps = null;
+		int rs = 0;
+
+		try {
+			con = conn.getConnection();
+
+			ps = con.prepareStatement("UPDATE Student SET firstName=':1', middleName=':1', lastName=':1', "
+					+ "username=':1', password=':1', langauge=':1' WHERE id=:1 ");
+			ps.setString(1, student.getFirstName());
+			ps.setString(2, student.getMiddleName());
+			ps.setString(3, student.getLastName());
+			ps.setString(4, student.getUsername());
+			ps.setString(5, student.getPassword());
+			ps.setString(6, student.getLangauge());
+			ps.setInt(7, student.getId());
+
+			rs = ps.executeUpdate();
+
+			if (rs == 1) {
+				return true;
+			} else {
+				return false;
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+			student.setId(-1);
+		} finally {
+			try {
+				if (ps != null)
+					ps.close();
+			} catch (Exception e) {
+			}
+			try {
+				if (con != null)
+					con.close();
+			} catch (Exception e) {
+			}
+		}
+
+		return false;
+	}
+
+	@Override
+	public boolean delete(Student student) {
+		Connection con = null;
+		PreparedStatement ps = null;
+		int rs = 0;
+
+		try {
+			con = conn.getConnection();
+
+			ps = con.prepareStatement("DELETE FROM Student WHERE id=:1 ");
+			ps.setInt(1, student.getId());
+			rs = ps.executeUpdate();
+			if (rs == 1) {
+				return true;
+			} else {
+				return false;
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+			student.setId(-1);
+		} finally {
+			try {
+				if (ps != null)
+					ps.close();
+			} catch (Exception e) {
+			}
+			try {
+				if (con != null)
+					con.close();
+			} catch (Exception e) {
+			}
+		}
+
+		return false;
+	}
+
+	@Override
+	public Student get(Student student) {
+
+		Connection con = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+
+		try {
+			con = conn.getConnection();
+
+			ps = con.prepareStatement("SELECT id, firstName, middleName, lastName, username, password, langauge"
+					+ " FROM Student WHERE id=:1 ");
+			ps.setInt(1, student.getId());
+			rs = ps.executeQuery();
+			if (rs.next()) {
+				student.setId(rs.getInt(1));
+				student.setFirstName(rs.getString(2));
+				student.setMiddleName(rs.getString(3));
+				student.setLastName(rs.getString(4));
+				student.setUsername(rs.getString(5));
+				student.setPassword(rs.getString(6));
+				student.setLangauge(rs.getString(7));
+			} else {
+				student.setId(0);
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+			student.setId(-1);
+		} finally {
+			try {
+				if (rs != null)
+					rs.close();
+			} catch (Exception e) {
+			}
+			try {
+				if (ps != null)
+					ps.close();
+			} catch (Exception e) {
+			}
+			try {
+				if (con != null)
+					con.close();
+			} catch (Exception e) {
+			}
+		}
+
+		return student;
+	}
+
+	@Override
+	public void list() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
