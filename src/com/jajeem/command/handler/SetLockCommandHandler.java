22135f50b5e169b7f233c859a18c3ad7a4f383a9
Mohammad <smh.tahaei@gmail.com>
2013-05-12 13:51:48 +0430

    lock mouse and keyboard handler

--- src/com/jajeem/command/handler/SetLockCommandHandler.java
+++ src/com/jajeem/command/handler/SetLockCommandHandler.java
@@ -1,32 +1,24 @@
 package com.jajeem.command.handler;
 
-import java.awt.FlowLayout;
-import java.awt.Font;
-import java.awt.Point;
-import java.awt.Rectangle;
-import java.awt.Toolkit;
-
-import javax.swing.JFrame;
-
-import com.alee.laf.label.WebLabel;
-import com.alee.laf.panel.WebPanel;
 import com.jajeem.command.model.Command;
+import com.jajeem.command.model.LockCommand;
+import com.jajeem.util.KeyboardBlocker;
+import com.jajeem.util.MouseBlocker;
 
 public class SetLockCommandHandler implements ICommandHandler {
 
+	private static MouseBlocker mouseBlocker = new MouseBlocker();
+	private static KeyboardBlocker keyboardBlocker = new KeyboardBlocker();
+
 	@Override
 	public void run(Command cmd) throws NumberFormatException, Exception {
-		JFrame frame = new JFrame();
-		WebLabel label = new WebLabel("LOCKED");
-		label.setFont(new Font("Serif", Font.PLAIN, 50));
-		WebPanel panel = new WebPanel(new FlowLayout(FlowLayout.CENTER));
-        panel.add(label);
-        frame.add(panel);
-		Toolkit tk = Toolkit.getDefaultToolkit();
-		frame.setBounds(new Rectangle(new Point(0, 0), tk.getScreenSize()));
-		frame.setUndecorated(true);
-		frame.setAlwaysOnTop(true);
-		frame.setVisible(true);
-	}
+		if (((LockCommand) cmd).isLock()) {
+			mouseBlocker.stop();
+			keyboardBlocker.stop();
 
-}
+		} else {
+				mouseBlocker.start();
+				keyboardBlocker.start();
+			}
+		}
+	}
