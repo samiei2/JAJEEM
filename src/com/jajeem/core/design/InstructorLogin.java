f51efa11a9a7b2aa09ef6ac18820b8a45262c225
Mohammad <smh.tahaei@gmail.com>
2013-05-21 17:10:15 +0430

    instructor login list

--- src/com/jajeem/core/design/InstructorLogin.java
+++ src/com/jajeem/core/design/InstructorLogin.java
@@ -1,71 +1,52 @@
 package com.jajeem.core.design;
 
-import info.clearthought.layout.TableLayout;
-
+import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.FlowLayout;
 import java.awt.Toolkit;
-import java.awt.Window;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
+import javax.swing.JButton;
 import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JSplitPane;
 import javax.swing.UIManager;
+import javax.swing.border.EmptyBorder;
 
-import com.alee.extended.panel.CenterPanel;
-import com.alee.extended.panel.GroupPanel;
+import org.apache.log4j.PropertyConfigurator;
+
 import com.alee.laf.WebLookAndFeel;
-import com.alee.laf.button.WebButton;
-import com.alee.laf.combobox.WebComboBox;
-import com.alee.laf.label.WebLabel;
-import com.alee.laf.panel.WebPanel;
+import com.alee.laf.list.DefaultListModel;
+import com.alee.laf.list.WebList;
 import com.alee.laf.rootpane.WebDialog;
+import com.alee.laf.scroll.WebScrollPane;
 import com.alee.laf.text.WebPasswordField;
-import com.alee.laf.text.WebTextField;
-import com.alee.managers.hotkey.Hotkey;
-import com.alee.managers.hotkey.HotkeyManager;
-import com.alee.utils.SwingUtils;
 import com.jajeem.core.service.InstructorService;
 import com.jajeem.room.model.Course;
 import com.jajeem.room.service.RoomService;
 import com.jajeem.util.Config;
 
-public class InstructorLogin extends JDialog {
+public class InstructorLogin extends WebDialog {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = -5121321476236877112L;
-
-	private static LoginDialog loginDialog;
-
-	final static WebTextField username = new WebTextField(15);
-	final static WebPasswordField password = new WebPasswordField(15);
-	final static WebComboBox courseCombo = new WebComboBox();
-
-	public static void setLoginDialogVisible(boolean flag) {
-		loginDialog.setVisible(flag);
-	}
-
-	public static void setFieldsColor(Color color) {
-		username.setBackground(color);
-		password.setBackground(color);
-	}
-
-	public static String getUsername() {
-		return username.getText();
-	}
+	private final JPanel contentPanel = new JPanel();
+	private final WebPasswordField password = new WebPasswordField(15);
 
 	/**
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
 		try {
-			UIManager.setLookAndFeel(WebLookAndFeel.class.getCanonicalName());
-
-			@SuppressWarnings("unused")
 			InstructorLogin dialog = new InstructorLogin();
+			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+			dialog.setVisible(true);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -74,112 +55,123 @@ public class InstructorLogin extends JDialog {
 	/**
 	 * Create the dialog.
 	 * 
-	 * @throws Exception
-	 * @throws NumberFormatException
+	 * @throws SQLException
 	 */
-	@SuppressWarnings("unchecked")
-	public InstructorLogin() throws NumberFormatException, Exception {
+	public InstructorLogin() throws SQLException {
 
-		new Config();
-		
-		// Enabling dialog decoration
+		setResizable(false);
+		setModal(true);
+		pack();
+		setTitle("Login to iCalabo");
+		setIconImage(Toolkit.getDefaultToolkit().getImage(
+				Instructor.class.getResource("/menubar/jajeem.jpg")));
 		boolean decorateFrames = WebLookAndFeel.isDecorateDialogs();
 		WebLookAndFeel.setDecorateDialogs(true);
-		
-		RoomService rs = new RoomService();
-		ArrayList<Course> courseList = rs.getCourseDAO().list();
-		
-		for (Course courseItem : courseList) {
-			courseCombo.addItem(courseItem.getName());
+
+		try {
+			// Setting up WebLookAndFeel style
+			UIManager.setLookAndFeel(WebLookAndFeel.class.getCanonicalName());
+
+			new Config();
+			PropertyConfigurator.configure("conf/log4j.conf");
+
+		} catch (Throwable e) {
 		}
 
-		// Opening dialog
-		loginDialog = new LoginDialog(this);
-		loginDialog.pack();
-		loginDialog.setLocationRelativeTo(this);
-		loginDialog.setVisible(true);
+		// get list of instructors
+		InstructorService instructorService = new InstructorService();
+		ArrayList<com.jajeem.core.model.Instructor> instructorList = instructorService
+				.list();
 
-		// Restoring frame decoration option
+		DefaultListModel listModel1 = new DefaultListModel();
+
+		for (com.jajeem.core.model.Instructor instructorItem : instructorList) {
+			listModel1.addElement(instructorItem.getUsername());
+		}
+
+		final WebList list1 = new WebList(listModel1);
+		WebScrollPane sPanel1 = new WebScrollPane(list1);
+		//
+
+		// get list of courses
+		RoomService rs = new RoomService();
+		ArrayList<Course> courseList = rs.getCourseDAO().list();
+		DefaultListModel listModel2 = new DefaultListModel();
+
+		for (Course courseItem : courseList) {
+			listModel2.addElement(courseItem.getName());
+		}
+
+		final WebList list2 = new WebList(listModel2);
+		WebScrollPane sPanel2 = new WebScrollPane(list2);
+		//
+
+		setBounds(100, 100, 450, 300);
+		getContentPane().setLayout(new BorderLayout());
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		contentPanel.setLayout(new BorderLayout(0, 0));
+		{
+			JSplitPane splitPane = new JSplitPane();
+			splitPane.setResizeWeight(0.5);
+			contentPanel.add(splitPane, BorderLayout.CENTER);
+			{
+				splitPane.setLeftComponent(sPanel1);
+			}
+			{
+				splitPane.setRightComponent(sPanel2);
+			}
+		}
+		{
+			JPanel panel = new JPanel();
+			contentPanel.add(panel, BorderLayout.SOUTH);
+			{
+				JLabel lblNewLabel = new JLabel("Password");
+				panel.add(lblNewLabel);
+			}
+			{
+				panel.add(password);
+
+			}
+		}
+		{
+			JPanel buttonPane = new JPanel();
+			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
+			getContentPane().add(buttonPane, BorderLayout.SOUTH);
+			{
+				JButton okButton = new JButton("Login");
+				okButton.setActionCommand("OK");
+				buttonPane.add(okButton);
+				getRootPane().setDefaultButton(okButton);
+
+				ActionListener listener = new ActionListener() {
+					@SuppressWarnings("static-access")
+					public void actionPerformed(ActionEvent e) {
+						try {
+							boolean grant = false;
+							InstructorService instructorService = new InstructorService();
+							grant = instructorService.authenticate(
+									(String) list1.getSelectedValue(),
+									password.getPassword());
+							if (grant) {
+								setVisible(false);
+								Instructor instructor = new Instructor();
+								instructor.main(new String[] { (String) list1.getSelectedValue(),
+										(String) list2.getSelectedValue() });
+								dispose();
+							} else {
+								password.setBackground(Color.decode("#FAD9D9"));
+							}
+						} catch (Exception e2) {
+							e2.printStackTrace();
+						}
+					}
+				};
+				okButton.addActionListener(listener);
+			}
+
+		}
 		WebLookAndFeel.setDecorateDialogs(decorateFrames);
 	}
 
-	public static class LoginDialog extends WebDialog {
-		/**
-		 * 
-		 */
-		private static final long serialVersionUID = -4106820035425545163L;
-		public static String name = "";
-
-		public LoginDialog(Window owner) throws SQLException {
-			super(owner, "Login to iCalabo");
-			setIconImage(Toolkit.getDefaultToolkit().getImage(
-					Student.class.getResource("/menubar/jajeem.jpg")));
-			setDefaultCloseOperation(WebDialog.DISPOSE_ON_CLOSE);
-			setResizable(false);
-			setModal(true);
-			setRound(0);
-			
-			TableLayout layout = new TableLayout(new double[][] {
-					{ TableLayout.PREFERRED, TableLayout.FILL },
-					{ TableLayout.PREFERRED, TableLayout.PREFERRED,
-							TableLayout.PREFERRED, TableLayout.PREFERRED } });
-			layout.setHGap(5);
-			layout.setVGap(5);
-			WebPanel content = new WebPanel(layout);
-			content.setMargin(15, 30, 15, 30);
-			content.setOpaque(false);
-
-			content.add(new WebLabel("Name", WebLabel.TRAILING), "0,0");
-			content.add(username, "1,0");
-
-			content.add(new WebLabel("Password", WebLabel.TRAILING), "0,1");
-			content.add(password, "1,1");
-
-			
-			content.add(courseCombo, "1,2");
-
-			WebButton login = new WebButton("Login");
-			login.setRound(0);
-			WebButton cancel = new WebButton("Cancel");
-			cancel.setRound(0);
-			ActionListener listener = new ActionListener() {
-				@SuppressWarnings("static-access")
-				public void actionPerformed(ActionEvent e) {
-					try {
-						boolean grant = false;
-						InstructorService instructorService = new InstructorService();
-						grant = instructorService.authenticate(username.getText(), password.getPassword());
-						if(grant) {
-							setVisible(false);
-							Instructor instructor = new Instructor();
-							instructor.main(new String[] {username.getText(), (String) courseCombo.getSelectedItem()});
-							dispose();
-						} else {
-							username.setBackground(Color.decode("#FAD9D9"));
-							password.setBackground(Color.decode("#FAD9D9"));
-						}
-					} catch (Exception e2) {
-						e2.printStackTrace();
-					}
-				}
-			};
-			login.addActionListener(listener);
-			cancel.addActionListener(new ActionListener() {
-
-				@Override
-				public void actionPerformed(ActionEvent e) {
-					setVisible(false);
-					dispose();
-				}
-			});
-			content.add(new CenterPanel(new GroupPanel(5, login, cancel)),
-					"0,3,1,3");
-			SwingUtils.equalizeComponentsWidths(login, cancel);
-
-			add(content);
-
-			HotkeyManager.registerHotkey(this, login, Hotkey.ESCAPE);
-			HotkeyManager.registerHotkey(this, login, Hotkey.ENTER);
-		}
-	}
 }
