a49874fe16e7e38c6971b154be8473f0122463f2
Mohammad <smh.tahaei@gmail.com>
2013-05-25 17:12:20 +0430

    db changes

--- src/com/jajeem/command/handler/SetBlackoutCommandHandler.java
+++ src/com/jajeem/command/handler/SetBlackoutCommandHandler.java
@@ -1,50 +1,41 @@
 package com.jajeem.command.handler;
 
-import java.awt.FlowLayout;
-import java.awt.Font;
-import java.awt.Point;
-import java.awt.Rectangle;
 import java.awt.Toolkit;
 
 import javax.swing.JFrame;
 
-import com.alee.laf.label.WebLabel;
-import com.alee.laf.panel.WebPanel;
 import com.jajeem.command.model.Command;
 import com.jajeem.core.design.Student;
 import com.jajeem.util.MouseBlocker;
+
 //import com.jajeem.util.KeyboardBlocker;
 
 public class SetBlackoutCommandHandler implements ICommandHandler {
 
 	private static MouseBlocker mouseBlocker = new MouseBlocker();
-//	private static KeyboardBlocker keyboardBlocker = new KeyboardBlocker();
+	// private static KeyboardBlocker keyboardBlocker = new KeyboardBlocker();
 	private static JFrame frame = new JFrame();
 
 	@Override
 	public void run(Command cmd) throws NumberFormatException, Exception {
 		if (Student.isBlack()) {
 			mouseBlocker.stop();
-//			keyboardBlocker.stop();
+			// keyboardBlocker.stop();
 			frame.setVisible(false);
 			frame.dispose();
 			Student.setBlack(false);
 
 		} else {
-				WebLabel label = new WebLabel("LOCKED");
-				label.setFont(new Font("Serif", Font.PLAIN, 50));
-				WebPanel panel = new WebPanel(new FlowLayout(FlowLayout.CENTER));
-				panel.add(label);
-				frame.add(panel);
-				Toolkit tk = Toolkit.getDefaultToolkit();
-				frame.setBounds(new Rectangle(new Point(0, 0), tk
-						.getScreenSize()));
-				frame.setUndecorated(true);
-				frame.setAlwaysOnTop(true);
-				frame.setVisible(true);
-				mouseBlocker.start();
-//				keyboardBlocker.start();
-				Student.setBlack(true);
-			}
+			frame = new JFrame("");
+			Toolkit tk = Toolkit.getDefaultToolkit();
+			int xSize = ((int) tk.getScreenSize().getWidth());
+			int ySize = ((int) tk.getScreenSize().getHeight());
+			frame.setSize(xSize, ySize);
+			frame.setUndecorated(true);
+			frame.setVisible(true);
+			mouseBlocker.start();
+			// keyboardBlocker.start();
+			Student.setBlack(true);
 		}
 	}
+}
