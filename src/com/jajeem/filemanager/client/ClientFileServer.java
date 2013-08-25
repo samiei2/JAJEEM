d04791b9116be8ef4db51e82e476c6dde71cb985
Mohammad <smh.tahaei@gmail.com>
2013-08-25 11:55:29 +0430

    opens received folder when received a file

--- src/com/jajeem/filemanager/client/ClientFileServer.java
+++ src/com/jajeem/filemanager/client/ClientFileServer.java
@@ -1,7 +1,9 @@
 package com.jajeem.filemanager.client;
 
+import java.awt.Desktop;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.ObjectInputStream;
 import java.net.ServerSocket;
@@ -17,15 +19,15 @@ import com.jajeem.util.FileUtil;
 import com.jajeem.util.StartUp;
 
 public class ClientFileServer {
-	public void Startup(){
-		
+	public void Startup() {
+
 		String inboxPath = FileUtil.getInboxPath();
 		String outboxPath = FileUtil.getOutboxPath();
 		File file = new File(outboxPath);
-		if(!file.exists())
+		if (!file.exists())
 			file.mkdirs();
 		file = new File(inboxPath);
-		if(!file.exists())
+		if (!file.exists())
 			file.mkdirs();
 		StartServer();
 	}
@@ -33,71 +35,84 @@ public class ClientFileServer {
 	private void StartServer() {
 		try {
 			ServerSocket ss = new ServerSocket(12345);
-			while(true){
+			while (true) {
 				final Socket client = ss.accept();
 				final ClientProgressWindow progwin = new ClientProgressWindow();
-		        new Thread(new Runnable() {
+				new Thread(new Runnable() {
 					@Override
 					public void run() {
-						try{
+						try {
 							InputStream in = client.getInputStream();
-						    
-						    byte[] path = new byte[2048];
-						    in.read(path, 0, 2048);
-						    String pathStr = new String(path);
-						    byte[] name = new byte[2048];
-						    in.read(name, 0, 2048);
-						    byte[] filelen = new byte[2048];
-						    in.read(filelen, 0, 2048);
-						    String temp = new String(filelen).trim();
-						    int fileLength = Integer.parseInt(temp.trim());
-						    
+
+							byte[] path = new byte[2048];
+							in.read(path, 0, 2048);
+							String pathStr = new String(path);
+							byte[] name = new byte[2048];
+							in.read(name, 0, 2048);
+							byte[] filelen = new byte[2048];
+							in.read(filelen, 0, 2048);
+							String temp = new String(filelen).trim();
+							int fileLength = Integer.parseInt(temp.trim());
+
 							String inboxPath = FileUtil.getInboxPath();
-						    String nameStr = new String(name);
-						    File inbox = new File(inboxPath);
-						    if(!inbox.exists())
-						    	inbox.mkdirs();
-						    File output = new File(inbox, nameStr);
-						    FileOutputStream fos = new FileOutputStream(output);
-						    
-						    int x=0;
-						    byte[] b = new byte[4194304];
-						    long bytesRead = 0;
-						    while((x = in.read(b)) > 0)
-						    {
-						        fos.write(b, 0, x);
-						        bytesRead += x;
-						        FileTransferObject evt = new FileTransferObject(this);
-						        evt.setProgressValue(((double)bytesRead/(double)fileLength)*100.0);
-						        new FileTransferEvent().fireProgress(evt, ClientProgressWindow.class);
-						    }
-						    fos.flush();
-						    fos.close();
-						    in.close();
-						    client.close();
-						    //TODO add filetransferobject object 
-						    progwin.dispose();
-						    FileTransferObject obj = new FileTransferObject(this);
-						    obj.setFileName(output.getAbsolutePath());
-						    new FileTransferEvent().fireSuccess(obj, ClientFileInbox.class);
-						    JOptionPane.showMessageDialog(null, "Teacher has sent you a file.you can find it in your inbox!");
-						}
-						catch(Exception e){
+							String nameStr = new String(name);
+							File inbox = new File(inboxPath);
+							if (!inbox.exists())
+								inbox.mkdirs();
+							File output = new File(inbox, nameStr);
+							FileOutputStream fos = new FileOutputStream(output);
+
+							int x = 0;
+							byte[] b = new byte[4194304];
+							long bytesRead = 0;
+							while ((x = in.read(b)) > 0) {
+								fos.write(b, 0, x);
+								bytesRead += x;
+								FileTransferObject evt = new FileTransferObject(
+										this);
+								evt.setProgressValue(((double) bytesRead / (double) fileLength) * 100.0);
+								new FileTransferEvent().fireProgress(evt,
+										ClientProgressWindow.class);
+							}
+							fos.flush();
+							fos.close();
+							in.close();
+							client.close();
+							// TODO add filetransferobject object
 							progwin.dispose();
-							JajeemExcetionHandler.logError(e,ClientFileServer.class);
-							new FileTransferEvent().fireFailure(null, ClientFileInbox.class);
+							FileTransferObject obj = new FileTransferObject(
+									this);
+							obj.setFileName(output.getAbsolutePath());
+							new FileTransferEvent().fireSuccess(obj,
+									ClientFileInbox.class);
+
+							File foler = new File(FileUtil.getInboxPath());
+							Desktop desktop = null;
+							if (Desktop.isDesktopSupported()) {
+								desktop = Desktop.getDesktop();
+							}
+							try {
+								desktop.open(foler);
+							} catch (IOException e) {
+							}
+						} catch (Exception e) {
+							progwin.dispose();
+							JajeemExcetionHandler.logError(e,
+									ClientFileServer.class);
+							new FileTransferEvent().fireFailure(null,
+									ClientFileInbox.class);
 						}
 					}
 				}).start();
-		        progwin.setVisible(true);
+				progwin.setVisible(true);
 			}
-	        
+
 		} catch (Exception e) {
-			JajeemExcetionHandler.logError(e,ClientFileServer.class);
+			JajeemExcetionHandler.logError(e, ClientFileServer.class);
 		}
 	}
-	
-	public static void main(String[] args){
+
+	public static void main(String[] args) {
 		new ClientFileServer().Startup();
 	}
 }
