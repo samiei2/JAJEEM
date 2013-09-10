b48344923f52303549a40f8d2e562af8c0e0d6d8
Mohammad <smh.tahaei@gmail.com>
2013-09-10 12:16:41 +0430

    Application starter modifications

--- src/com/jajeem/command/handler/StartApplicationCommandHanlder.java
+++ src/com/jajeem/command/handler/StartApplicationCommandHanlder.java
@@ -1,6 +1,9 @@
 package com.jajeem.command.handler;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 
 import com.jajeem.command.model.Command;
 import com.jajeem.command.model.StartApplicationCommand;
@@ -19,20 +22,48 @@ public class StartApplicationCommandHanlder implements ICommandHandler {
 				+ "\\Programs";
 
 		FileUtil fileUtil = new FileUtil();
-		final File[] fileList = fileUtil.finder(pathToStartMenu);
+		File[] fileList = fileUtil.finder(pathToStartMenu);
 
-		for (int i = 0; i < fileList.length; i++) {
+		File[] tempfileList = fileUtil.finder(pathToStartMenu);
+		ArrayList<File> listOfAllLinks = new ArrayList<>();
+		for (int i = 0; i < tempfileList.length; i++) {
+			if (tempfileList[i].isDirectory())
+				listOfAllLinks
+						.addAll(getPath(getDirectoryContent(tempfileList[i])));
+			else
+				listOfAllLinks.add(tempfileList[i]);
+		}
 
-			File file = fileList[i];
-			if (file.getName().indexOf(".") != -1) {
-				if (file.getName().substring(0, file.getName().length() - 4)
-						.equals(((StartApplicationCommand) cmd).getFileName())) {
-					ProcessBuilder pb = new ProcessBuilder("cmd", "/c",
-							file.getPath());
-					pb.start();
-					break;
-				}
+		for (int i = 0; i < listOfAllLinks.size(); i++) {
+			File file = listOfAllLinks.get(i);
+			if (file.getPath().contains(
+					((StartApplicationCommand) cmd).getFileName())) {
+
+				ProcessBuilder pb = new ProcessBuilder("cmd", "/c",
+						file.getPath());
+				pb.start();
+				break;
+
 			}
 		}
+	}
+
+	protected ArrayList<File> getDirectoryContent(File file) {
+		ArrayList<File> files = new ArrayList<>(Arrays.asList(file.listFiles()));
+		for (int i = 0; i < files.size(); i++) {
+			if (files.get(i).isDirectory()) {
+				files.addAll(getDirectoryContent(files.get(i)));
+			}
+		}
+		return files;
+	}
+
+	protected Collection<? extends File> getPath(
+			ArrayList<File> directoryContent) {
+		ArrayList<File> list = new ArrayList<>();
+		for (int i = 0; i < directoryContent.size(); i++) {
+			list.add(directoryContent.get(i));
+		}
+		return list;
 	}
 }
