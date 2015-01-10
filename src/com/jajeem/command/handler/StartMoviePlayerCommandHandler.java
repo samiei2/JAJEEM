32c4ac16aa2079d40aba752559cefe59ddeca613
armin <samiei2@gmail.com>
2015-01-10 19:46:01 +0330

    new solution for executing vlc because of delays starting it

--- src/com/jajeem/command/handler/StartMoviePlayerCommandHandler.java
+++ src/com/jajeem/command/handler/StartMoviePlayerCommandHandler.java
@@ -5,6 +5,9 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Paths;
 
+import org.apache.commons.exec.CommandLine;
+import org.apache.commons.exec.DefaultExecutor;
+
 import com.jajeem.command.model.Command;
 import com.jajeem.command.model.StartMoviePlayerCommand;
 import com.jajeem.command.model.StopMoviePlayerCommand;
@@ -18,15 +21,20 @@ public class StartMoviePlayerCommandHandler implements ICommandHandler {
 		// TestPlayer player = new
 		// TestPlayer(command.getStreamAddress(),command.isClient());
 		// Run a java app in a separate system process
-		Process proc = null;
+//		Process proc = null;
 		try {
-			System.out.println("Mrl: "+command.getMrl());
+//			System.out.println("Mrl: "+command.getMrl());
 			String path=System.getProperty("user.dir");
-			System.out.println(System.getProperty("user.dir"));
-			ProcessBuilder builder = new ProcessBuilder(path+"/util/vlcwin/vlc.exe","-vvv",command.getMrl());
-			proc = builder.start();
-		} catch (IOException e) {
-			e.printStackTrace();
+//			System.out.println(System.getProperty("user.dir"));
+//			ProcessBuilder builder = new ProcessBuilder(path+"/util/vlcwin/vlc.exe","-vvv",command.getMrl());
+//			proc = builder.start();
+			String line = path+"/util/vlcwin/vlc.exe"+" -vvv "+command.getMrl();
+			CommandLine cmdLine = CommandLine.parse(line);
+			DefaultExecutor executor = new DefaultExecutor();
+			executor.setExitValue(1);
+			int exitValue = executor.execute(cmdLine);
+		} catch (Exception e) {
+//			e.printStackTrace();
 		}
 		// Then retreive the process output
 //		InputStream in = proc.getInputStream();
--- src/com/jajeem/command/handler/StopMoviePlayerCommandHandler.java
+++ src/com/jajeem/command/handler/StopMoviePlayerCommandHandler.java
@@ -1,15 +1,27 @@
 package com.jajeem.command.handler;
 
+import org.apache.commons.exec.CommandLine;
+import org.apache.commons.exec.DefaultExecutor;
+
 import com.jajeem.command.model.Command;
-import com.jajeem.command.model.StopMoviePlayerCommand;
 
 public class StopMoviePlayerCommandHandler implements ICommandHandler {
 
 	@SuppressWarnings("unused")
 	@Override
 	public void run(Command cmd) throws NumberFormatException, Exception {
-		StopMoviePlayerCommand command = (StopMoviePlayerCommand) cmd;
-		ProcessBuilder builder = new ProcessBuilder("taskkill", "/IM", "vlc.exe" );
-		builder.start();
+//		StopMoviePlayerCommand command = (StopMoviePlayerCommand) cmd;
+//		ProcessBuilder builder = new ProcessBuilder("taskkill", "/IM", "vlc.exe" );
+//		builder.start();
+		try{
+			String line = "taskkill"+" /IM "+"vlc.exe";
+			CommandLine cmdLine = CommandLine.parse(line);
+			DefaultExecutor executor = new DefaultExecutor();
+			executor.setExitValue(1);
+			int exitValue = executor.execute(cmdLine);
+		}
+		catch(Exception e){
+			
+		}
 	}
 }
